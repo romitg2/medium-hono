6a4b154cc0ad43d5e44663cf00f871465c8d744b
romit <romitgabani1@gmail.com>
2024-10-11 22:57:12 +0530

    new start

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,47 @@
 import { Hono } from "hono";
+import { decode, sign, verify } from "hono/jwt";
 import { User } from "@prisma/client";
+import { userRouter } from "./routes/user";
+
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
 
 const app = new Hono<{
   Bindings: {
     DATABASE_URL: string;
+    JWT_SECRET: string;
   };
 }>();
 
-app.get("/", (c) => {
+app.route("/api/v1/user/signup", userRouter);
+
+app.get("/", async (c) => {
+  const body = await c.req.json();
+
   const prisma = new PrismaClient({
     datasourceUrl: c.env.DATABASE_URL,
   }).$extends(withAccelerate());
+
+  try {
+    const user = prisma.user.create({
+      data: {
+        name: body.username,
+        password: body.password,
+      },
+    });
+
+    const jwt = await sign(
+      {
+        id: user.id;
+      },
+      c.env.JWT_SECRET,
+    );
+    return c.text(jwt);
+
+  } catch (e) {
+    c.status(400);
+    return c.json({ message: "User already exists" });
+  }
 
   return c.text("Hello World");
 });
---
+++ src/routes/user.ts
@@ -0,0 +1,5 @@
+import { Hono } from "hono";
+
+const userRouter = new Hono();
+
+export { userRouter };
